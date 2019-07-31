c6ac2d80405d0e0e959c949a69b9a0ff9b0b81ae
nathankong <kongnathan@gmail.com>
2019-07-31 16:52:43 -0700

    Initial commit

---
+++ runReliabilityP.m
@@ -0,0 +1,30 @@
+% Test script for reliabilities
+
+addpath(genpath('.'));
+
+% Pantazis data
+load('/home/groups/amnorcia/eeg_data/pantazis_data/pantazis_eeg.mat');
+
+% Permute dimensions so (num_images, num_subjects, num_trials, num_timepoints, num_components)
+eeg_data = permute(eeg_data, [2,1,3,5,4]);
+
+num_images = size(eeg_data,1);
+num_subjects = size(eeg_data,2);
+num_trials = size(eeg_data,3);
+num_timepoints = size(eeg_data,4);
+num_components = size(eeg_data,5);
+
+eeg_data = reshape(eeg_data, [num_images, num_subjects*num_trials, num_timepoints, num_components]);
+eeg_data = reshape(eeg_data, [num_images*num_subjects*num_trials, num_timepoints, num_components]);
+labels = repmat(1:72, [1,num_subjects*num_trials]);
+disp(size(eeg_data));
+disp(size(labels));
+
+eeg_data = permute(eeg_data, [3,1,2]);
+
+%rel_time = computeSpaceTimeReliability(eeg_data, labels, 10);
+%save('results/rel_time_p_31Jul19.mat', 'rel_time');
+
+rel_trials = computeSampleSizeReliability(eeg_data, labels, 16, 1:100, 10, 100);
+save('results/rel_trials_p_16_31Jul19.mat', 'rel_trials');
+
---
+++ runReliabilityTimePerSubjectP.m
@@ -0,0 +1,34 @@
+% Test script for reliabilities
+
+addpath(genpath('.'));
+
+% Pantazis data
+load('/home/groups/amnorcia/eeg_data/pantazis_data/pantazis_eeg.mat');
+
+% Permute dimensions so (num_images, num_subjects, num_trials, num_timepoints, num_components)
+eeg_data = permute(eeg_data, [2,1,3,5,4]);
+
+num_images = size(eeg_data,1);
+num_subjects = size(eeg_data,2);
+num_trials = size(eeg_data,3);
+num_timepoints = size(eeg_data,4);
+num_components = size(eeg_data,5);
+
+all_subj = zeros(num_subjects, num_timepoints, 10, num_components);
+for i=1:num_subjects
+    fprintf('Subject %d\n', i);
+    eeg_data_subj = squeeze(eeg_data(:,i,:,:,:));
+    eeg_data_subj = reshape(eeg_data_subj, [num_images*num_trials, num_timepoints, num_components]);
+    labels = repmat(1:72, [1,num_trials]);
+
+    disp(size(eeg_data));
+    disp(size(labels));
+
+    eeg_data_subj = permute(eeg_data_subj, [3,1,2]);
+    
+    rel_time = computeSpaceTimeReliability(eeg_data_subj, labels, 10);
+    all_subj(i,:,:,:) = rel_time;
+end
+
+save('results/all_subj_p_31Jul19.mat', 'all_subj');
+
